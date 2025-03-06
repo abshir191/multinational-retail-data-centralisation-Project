cbf21e8583c037a1beee7f84dfacc05d258cbe17
Abdirahman <a.abshir199@gmail.com>
2025-03-06 17:51:01 +0000

    Added Milestone3 sql queries to github

--- Milestone3_Database_schema.sql
+++ Milestone3_Database_schema.sql
@@ -1,4 +1,6 @@
 --Milestone 3
+
+--Cast orders_table columns to correct data types
 ALTER TABLE orders_table 
 ALTER COLUMN date_uuid SET DATA TYPE UUID USING date_uuid::UUID, 
 ALTER COLUMN user_uuid SET DATA TYPE UUID USING user_uuid::UUID,
@@ -7,6 +9,7 @@ ALTER COLUMN store_code TYPE VARCHAR(12) USING store_code::VARCHAR(12),
 ALTER COLUMN product_code TYPE VARCHAR(11) USING product_code::VARCHAR(11),
 ALTER COLUMN product_quantity TYPE SMALLINT USING product_quantity::SMALLINT;
 
+--Cast dim_users columns to correct data types
 ALTER TABLE dim_users
 ALTER COLUMN first_name TYPE VARCHAR(255) USING first_name::VARCHAR(255)
 ALTER COLUMN last_name TYPE VARCHAR(255) USING last_name::VARCHAR(255),
@@ -15,6 +18,7 @@ ALTER COLUMN country_code TYPE VARCHAR(3) USING country_code::VARCHAR(3),
 ALTER COLUMN user_uuid TYPE UUID USING user_uuid::uuid,
 ALTER COLUMN join_date TYPE DATE USING join_date::DATE,
 
+--update dim_store_details table
 ALTER TABLE dim_store_details
 ALTER COLUMN longitude TYPE NUMERIC USING longitude::NUMERIC
 ALTER COLUMN latitude TYPE NUMERIC USING latitude::NUMERIC,
@@ -26,6 +30,7 @@ ALTER COLUMN store_type TYPE VARCHAR(255) USING store_type::VARCHAR(255),
 ALTER COLUMN country_code TYPE VARCHAR(10) USING country_code::VARCHAR(10),
 ALTER COLUMN continent TYPE VARCHAR(255) USING continent::VARCHAR(255);
 
+--Make changes to dim_products for delivery team
 UPDATE dim_products SET product_price = REPLACE(product_price, '£', '');
 ALTER TABLE dim_products
 RENAME COLUMN removed TO still_available;
@@ -39,7 +44,7 @@ SET weight_class =
         WHEN weight::FLOAT >= 40 AND weight::FLOAT < 140 THEN 'Heavy'
         WHEN weight::FLOAT >= 140 THEN 'Truck_Required'
     END;
-
+--update dim_products table with required data types
 ALTER TABLE dim_products
 ALTER COLUMN weight_class TYPE VARCHAR(14) USING weight_class::VARCHAR(14)
 ALTER COLUMN product_price TYPE NUMERIC USING product_price::NUMERIC,
@@ -54,12 +59,14 @@ ALTER COLUMN still_available TYPE BOOL
     ELSE TRUE
   END;
 
+--update dim_date_times table
 ALTER TABLE dim_date_times
 ALTER COLUMN month TYPE VARCHAR(10) USING month::VARCHAR(10)
 ALTER COLUMN year TYPE VARCHAR(10) USING year::VARCHAR(10),
 ALTER COLUMN day TYPE VARCHAR(10) USING day::VARCHAR(10),
 ALTER COLUMN time_period TYPE VARCHAR(10) USING time_period::VARCHAR(10);
 
+--update dim_card_details
 ALTER TABLE dim_card_details
 ALTER COLUMN card_number TYPE VARCHAR(22) USING card_number::VARCHAR(22),
 ALTER COLUMN expiry_date TYPE VARCHAR(5) USING expiry_date::VARCHAR(5),
@@ -67,7 +74,7 @@ ALTER COLUMN  date_payment_confirmed TYPE DATE USING  date_payment_confirmed::DA
 
 
 --Primary keys
-
+--Create the primary keys in the dimension tables
 ALTER TABLE dim_users
 ADD PRIMARY KEY (user_uuid);
 
@@ -87,6 +94,7 @@ ADD PRIMARY KEY (card_number);
 
 
 --Foreign keys
+--finalising the star_based schema & adding the foreign keys to the orders table
 ALTER TABLE orders_table
 ADD CONSTRAINT store_code_fkey
 FOREIGN KEY (store_code) REFERENCES dim_store_details(store_code),
