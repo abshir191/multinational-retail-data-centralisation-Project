f4116fe508a95ffaab4782d6a31256597ce18642
Abdirahman <a.abshir199@gmail.com>
2025-03-08 16:46:46 +0000

    Updated Extractor module

--- DataExtractor.py
+++ DataExtractor.py
@@ -6,6 +6,7 @@ import requests
 import boto3
 from io import StringIO
 import time
+import os
 
 class DataExtractor:
     @staticmethod
@@ -41,39 +42,52 @@ class DataExtractor:
                 #print ("stores data:", stores_data)
                 return stores_data
             else:
-                return f"Error:{response.status_code}:{response.text}"
+                return f"Error 1:{response.status_code}:{response.text}"
         except requests.exceptions.RequestException as e:
-            print(f"Error:{e}")
+            print(f"Error2:{e}")
 
 #extracts all the stores from the API saving them in a pandas DataFrame.
     @staticmethod
-    def retrieve_stores_data(return_stores_endpoint, headers, num_stores):
+    def retrieve_stores_data(return_stores_endpoint, headers, num_stores, csv_file = "stores_data.csv"):
+
+        if os.path.exists(csv_file):
+            print(f"Loading data from existing file: {os.path.abspath(csv_file)}")
+            return pd.read_csv(csv_file)
+        print("Fetching data from API...")
+       
         extracted_stores = []
 
         for stores_num in range(0, num_stores):
-            store_url = f"{return_stores_endpoint}/{stores_num}"     
-            start_time = time.time()    
-            response = requests.get(store_url, headers=headers, timeout=15)
-            end_time = time.time()
+            store_url = f"{return_stores_endpoint}/{stores_num}"       
+            response = requests.get(store_url, headers=headers)
 
             if response.status_code == 200:
-                store_data = response.json()  
-                extracted_stores.extend(store_data)
+                store_data = response.json()
+                #print(f"Store Data for {stores_num}: {store_data}") 
+
+                if isinstance (store_data, dict):
+                    extracted_stores.append(store_data)
+                else:
+                    print(response.text)
+                    raise Exception( f"Error3: {response.status_code}, {response.text}")
             else:
-                print(response.text)
-                raise Exception( f"Error: {response.status_code}, {response.text}")
+                print(f"Failed to fetch store data for {stores_num}: {response.status_code}")
 
         stores_df = pd.DataFrame(extracted_stores)
+        if stores_df.empty:
+            raise ValueError("Retrieved data is empty. Please check the API response.")
+        
         return stores_df
         
 
     @staticmethod
     def extract_from_s3(s3_key):
-        s3 = boto3.client('s3')
+        s3 = boto3.client('s3', region_name = 'eu-west-1')
         s3_bucket = s3_key.split('/')[2]
         file_key = '/'.join(s3_key.split('/')[3:])
+        print(f"Bucket: {s3_bucket}, File: {file_key}")
 
-        load_s3_data = s3.get_object(Bucket = s3_bucket, Key = file_key)
+        load_s3_data = s3.get_object(Bucket = 'data-handling-public', Key = 'products.csv')
         raw_s3_data = load_s3_data['Body'].read().decode('utf-8')
 
         #check if file extension is csv
@@ -85,35 +99,5 @@ class DataExtractor:
             extracted_s3_data = pd.read_json(StringIO(raw_s3_data))
             return extracted_s3_data
         else:
-            print(f"Error: {file_ext} is not recognised")
             return None
-        #s3_key = "s3://data-handling-public/products.csv"
-        #dataframe = DataExtractor.extract_from_s3(s3_key)
-        #if dataframe is not None:
-        #    print(dataframe.head())
-        
     
-    
-    
-
-
-            
-
-if __name__ == "__main__":
-    utils = database_utils.DatabaseConnector()
-    engine = utils.init_db_engine("C:\\Users\\abshi\\OneDrive\\Documents\\IT\\Multination Retail Data Centralisation Project\\db_creds.yaml")
-    #get_db_tables = utils.list_db_tables this was a mistake as i called it using an instance which isn't necessarry beacuse i used a static method, meaning i can jus call it directly. done below:
-    get_db_tables = DataExtractor.list_db_tables(engine)
-    table_name = 'legacy_users'
-    read_rds = DataExtractor.read_rds_table(engine, table_name)
-    #link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
-    retrieve_pdf = DataExtractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
-    headers = {"x-api-key":"yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
-    store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details'
-    return_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
-    DataExtractor.list_number_of_stores(return_stores_endpoint, headers)
-    num_stores = 451
-    DataExtractor.retrieve_stores_data(store_endpoint,headers,num_stores)
-    s3_key = 'products.csv'
-    orders_df = DataExtractor.read_rds_table(engine,'orders_table')
-    data_address = "https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json."
